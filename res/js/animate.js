cd71cb5ed005e2cbd87f2f08884f24e48d38103d
amias-burrows <amias.burrows@gmail.com>
2021-07-19 14:18:49 +0100

    desktop version fixed

--- res/js/animate.js
+++ res/js/animate.js
@@ -1,12 +1,13 @@
 var cover = document.getElementById('cover');
 cover.addEventListener('click', flip);
 var title = document.getElementById('title');
-title.addEventListener('click', flip);
 var content = document.getElementsByTagName('section')[0];
 var footer = document.getElementsByTagName('footer')[0];
+var scroll = document.getElementById('arrow');
 
 function flip() {
 	if (cover.classList.contains('flipped')) {
+		cover.removeEventListener('click', flip);
 		cover.classList.remove('flipped');
 
 		cover.classList.add('show');
@@ -21,9 +22,15 @@ function flip() {
 			setTimeout(function() {
 				title.classList.add('show');
 				title.classList.remove('flipped');
+				setTimeout(function() {
+					title.addEventListener('click', flip);
+					scroll.style.display = "block";
+				}, 1000);
 			}, 10);
-		}, 1500);
+		}, 1000);
 	} else {
+		scroll.style.display = "none";
+		title.removeEventListener('click', flip);
 		title.classList.remove('show');
 
 		content.style.display = 'none';
@@ -38,7 +45,10 @@ function flip() {
 			setTimeout(function() {
 				cover.classList.remove('show');
 				cover.classList.add('flipped');
+				setTimeout(function() {
+					cover.addEventListener('click', flip);
+				}, 1000);
 			}, 10);
-		}, 1500);
+		}, 1000);
 	}
 }
